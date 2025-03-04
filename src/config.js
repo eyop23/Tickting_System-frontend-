033d150e498b5a8843c6a5a05a27958f34500f06
Eyob Mekonnen <emekonnen35@gmail.com>
2025-03-04 09:44:26 +0300

    change the base url to the deployed backend url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // src/config.js
 
-const BASE_URL = "http://localhost:8000/api";  // Change this URL if your backend is deployed elsewhere
-
+// const BASE_URL = "http://localhost:8000/api";  // Change this URL if your backend is deployed elsewhere
+const BASE_URL = "https://tickting-system-backend.onrender.com/api"; // Use the deployed URL for the backend
 export default BASE_URL;
